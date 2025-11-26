b904bc06fbaee7cc0a5129a3a585b0eba8d661fa
gowtham809 <gowtham.selvam@rhibhus.com>
2025-11-27 00:33:31 +0530

    feat: create login and register screens with validation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,172.26.80.1').split(',')
 
 
 # Application definition
